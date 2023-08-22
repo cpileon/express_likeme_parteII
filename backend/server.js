9577c2f934675937f91b6403183f08b8b5f3ffdf
Constanza Pino <cpino012@gmail.com>
2023-08-21 20:21:19 -0400

    Added Try-catch

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const { handleErrors } = require('./errors')
 const { obtenerPosts, agregarPost, borrarPost, modificarLikes } = require('./consultas')
 const express = require('express')
 const app = express()
@@ -10,25 +11,52 @@ app.use(cors())
 
 
 app.get("/posts", async (req, res) => {
+    try{
     const posts = await obtenerPosts()
     res.json(posts)
+    //respuesta del servidor
+    }catch (error) {
+        console.log(error)
+        const { status, message } = handleErrors(error.code)
+        return res.status(status).json({ok: false, result: message})
+    }
 })
 
 app.post("/posts", async (req, res) => {
-    const { titulo, url, descripcion } = req.body
-    await agregarPost(titulo, url, descripcion)
-    res.send("Post agregado con éxito")
+    try{
+        const { titulo, url, descripcion } = req.body
+        await agregarPost(titulo, url, descripcion)
+        res.send("Post agregado con éxito")
+    }catch (error) {
+        console.log(error)
+        const { status, message } = handleErrors(error.code)
+        return res.status(status).json({ok: false, result: message})
+    }
 })
 
 app.delete("/posts/:id", async (req,res) =>{
-    const { id } = req.params
-    await borrarPost(id)
-    res.send("Post eliminado con éxito")
+    try{
+        const { id } = req.params
+        await borrarPost(id)
+        res.send("Post eliminado con éxito")
+    }catch (error) {
+        console.log(error)
+        const { status, message } = handleErrors(error.code)
+        return res.status(status).json({ok: false, result: message})
+    }
 })
 
+
 app.put("/posts/:id", async (req, res) => {{
-}   const { id } = req.params
-    let { likes } = req.query
-    await modificarLikes(likes, id)
-    res.send("Likes actualizados con éxito")
+}   
+    try{
+        const { id } = req.params
+        let { likes } = req.query
+        await modificarLikes(likes, id)
+        res.send("Likes actualizados con éxito")
+    }catch (error) {
+        console.log(error)
+        const { status, message } = handleErrors(error.code)
+        return res.status(status).json({ok: false, result: message})
+    }
 })
